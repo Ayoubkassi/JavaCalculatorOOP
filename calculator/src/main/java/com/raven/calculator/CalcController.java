35ce2f0202a8792ba1d5b380c9d45d6f6dc83a6f
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2022-10-30 12:34:18 +0100

    change to Labels and add current val

--- calculator/src/main/java/com/raven/calculator/CalcController.java
+++ calculator/src/main/java/com/raven/calculator/CalcController.java
@@ -4,10 +4,28 @@
  */
 package com.raven.calculator;
 
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 /**
  *
  * @author ryota
  */
 public class CalcController {
+    @FXML
+    private TextField textField;
     
+    @FXML
+    private void loadWhenClicked(ActionEvent event){
+        Button button = (Button) event.getSource();
+        String actualVal = button.getText();
+        
+        String previousVal = textField.getText();
+        String current = previousVal.concat(actualVal);
+        textField.setText(current);
+        
+    }
 }
